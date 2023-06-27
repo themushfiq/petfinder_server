99cadc7d44c9957af550c5bbc4fe2112ce74dda1
ShakilJoy31 <joy31a@gmail.com>
2023-06-28 01:41:57 +0600

    Done the backend for tradesent

--- index.js
+++ index.js
@@ -44,6 +44,9 @@ app.post("/create-user", async (req, res) => {
   console.log(result);
   res.send(result);
 });
+
+
+
 app.get("/all-user", async (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
@@ -53,32 +56,15 @@ app.get("/all-user", async (req, res) => {
   console.log(result);
   res.send(result);
 });
+
+
+
 app.put("/update-user", async (req, res) => {
   const { userId } = req.query;
   const formData = req.body;
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  const currentDate = new Date();
-  const currentDay = currentDate.getDate();
-  const document = await userCollection.findOne({_id: new ObjectId(userId)});
-    if (document) {
-      // Check if the document has been updated today
-      const lastUpdatedDay = document.lastUpdated?.getDate();
-      if (lastUpdatedDay !== currentDay) {
-        // Decrement the field value and set the lastUpdated field to the current date
-        const update = {
-          $inc: { verifiedDaysRemaining: -1 }, // Replace 'making' with the field you want to decrement
-          $set: { lastUpdated: currentDate },
-        };
-        await userCollection.findOneAndUpdate({_id: new ObjectId(userId)}, update);
-        console.log('Field value decremented successfully');
-      } else {
-        console.log('Field already updated today');
-      }
-    } else {
-      console.log('Document not found');
-    }
   const result = userCollection.findOneAndUpdate(
     { _id: new ObjectId(userId) },
     { $set: formData }
@@ -93,18 +79,3 @@ app.listen(port, () => {
 app.get("/", (req, res) => {
   res.send("This project is running.");
 });
-
-// async function databaseConnection () {
-//     try{
-
-//         await mongoose.connect("mongodb+srv://logedInUserr:HCtwliFQhB9gD7m1@cluster0.qzz0mz8.mongodb.net/?retryWrites=true&w=majority");
-//         console.log("Database is connected successfully. Thank you.");
-//         app.listen(port, ()=>{
-//             console.log(`Task app listening on ${port}`)
-//         })
-
-//     }catch(error){
-//         console.log( (error).message);
-//     }
-// }
-// databaseConnection();
