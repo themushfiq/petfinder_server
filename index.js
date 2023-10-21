f3f607479b15b72fbf85625572f41dbeec17453e
ShakilJoy31 <joy31a@gmail.com>
2023-10-21 15:13:48 +0600

    port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import { MongoClient, ObjectId, ServerApiVersion } from "mongodb";
 const app = express();
 app.use(cors());
 const port = 7000;
-// const nodeEnv = process.env.MONGO_URI;
+// const nodeEnv = process.env.MONGO_URI; 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
